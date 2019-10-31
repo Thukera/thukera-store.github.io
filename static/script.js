f53a63ea27abfa41da60cb53f4d6afb19c7e54f3
Thukera <31145663+Thukera@users.noreply.github.com>
2019-10-30 23:58:21 -0300

    My First commit

---
+++ static/script.js
@@ -0,0 +1,57 @@
+
+
+/*Navbar Produto Vertical ScrolSpy Active*/
+$('#navBarVertical').on('activate.bs.scrollspy', function () {
+    
+  })
+
+
+
+/*Initialize Swiper Slide*/
+
+var swiper = new Swiper('.slide-preview', {
+    slidesPerView: 5,
+    spaceBetween: 50,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    breakpoints: {
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        }
+    }
+});
+
+/*Initialize Swiper Galery Slide*/
+
+var galleryThumbs = new Swiper('.gallery-thumbs', {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+  });
+  var galleryTop = new Swiper('.gallery-top', {
+    spaceBetween: 10,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+      swiper: galleryThumbs
+    }
+  });
